35b8f879dcd71c2f5e5902440186c9d62a83da55
morgansullivan <morgan.siobhan0@gmail.com>
2016-04-22 00:28:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,46 +11,88 @@ console.log("Question 1");
 console.log('Morgan');
 
 //2. Declare a variable called 'name', and then log it to the console.
+console.log("Question 2");
 var name = 'Morgan';
 console.log(name);
 
 //3. Declare two variables with number values. 
 console.log("Question 3")
-console.log("var x = 5;")
+var x = 5;
+var y = 5;
+console.log(x,y);
 
 //   Add, subtract, multiply and divide them.
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
 
-x += 3;
-x -= 3;
-x *= 3;
-x /= 3;
 
 //4. Declare two variables with string values. 
-var loc = "Seattle";
-var axn = "living";
+
+console.log("Question 4");
+var loc = 'Seattle';
+var axn = 'living ';
 
 //   Create a third variable that concatenates them.
 
-var output = 'I am' + axn + 'in' + loc + '.'
-console.log('output')
+var output = 'I am ' + axn + 'in ' + loc + '.';
+console.log(output)
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. 
+console.log("Question 5")
+console.log("5"+"3")
+console.log("5"-"3")
+console.log("5"*"3")
+console.log("5"/"3")
+
 //   Observe the results and know how to explain the results if asked.
+console.log("All of them worked the way they should, expect for the addition - which becomes a string value. Honestly, I have no idea why that's happening.")
 
 //6. Create a prompt that asks a user to pick a number. 
+
+console.log("Question 6");
+var picknumber = prompt("I'm a pop-up!", "Pick a number...");
+
+
 //   Then double the answer.
+
+var picknumber = prompt("I'm a pop-up!", "Pick a number...");
+
+var y = 2; 
+
+console.log(picknumber*y)
+
 
 //7. Create a custom alert.
 
 //8. Declare three variables called length, width and height. 
 //   Calculate area and volume.
+console.log("Question 8");
+
+var length = 2;
+var width = 3;
+var height = 4;
+
+console.log(length*width*height)
 
 //9. Ask for a user's name, age and city. 
+
+console.log("Question 9");
+
+var name = prompt("I'm a pop-up!", "What's your name?");
+
+var age = prompt("I'm a pop-up!", "How old are you?");
+
+var city = prompt("I'm a pop-up!", "Where are you from?");
+
+console.log("My name is " + name + ", I'm " + age + " years old, and I live in " + city + ".");
+
 //   In a single command display/log the sentence:
 //   "My name is...I'm....years old, and I live in....."
 
 //10. Create the formula to determine the area of a rectangle. The rectangle is: 2inches by 3 inches
-
+console.log("Question 10");
 var height = 2;
 var width = 3;
 var area = height * width;
