0bb06f0695e02f18ee6e1e4ebeb09cbefb1a957f
morgansullivan <morgan.siobhan0@gmail.com>
2016-04-21 18:49:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,23 +8,31 @@
 //1. Log your name to the console (as a string).
 
 console.log("Question 1");
-console.log(Morgan);
+console.log('Morgan');
 
 //2. Declare a variable called 'name', and then log it to the console.
 var name = 'Morgan';
 console.log(name);
 
 //3. Declare two variables with number values. 
-var x = 5;
+console.log("Question 3")
+console.log("var x = 5;")
+
+//   Add, subtract, multiply and divide them.
+
 x += 3;
 x -= 3;
 x *= 3;
 x /= 3;
 
-//   Add, subtract, multiply and divide them.
-
 //4. Declare two variables with string values. 
+var loc = "Seattle";
+var axn = "living";
+
 //   Create a third variable that concatenates them.
+
+var output = 'I am' + axn + 'in' + loc + '.'
+console.log('output')
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. 
 //   Observe the results and know how to explain the results if asked.
@@ -41,6 +49,11 @@ x /= 3;
 //   In a single command display/log the sentence:
 //   "My name is...I'm....years old, and I live in....."
 
-//10. Create an alert that uses the response from a prompt.
+//10. Create the formula to determine the area of a rectangle. The rectangle is: 2inches by 3 inches
+
+var height = 2;
+var width = 3;
+var area = height * width;
+console.log(area);
 
 
